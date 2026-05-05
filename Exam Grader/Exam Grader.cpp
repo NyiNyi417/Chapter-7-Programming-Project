7e24490a10acfca8c0ee5e8631096941b07709f8
nydia <nzw25489@email.vccs.edu>
2026-05-05 19:25:21 -0400

    Add project files.

---
+++ Exam Grader/Exam Grader.cpp
@@ -0,0 +1,16 @@
+// Exam Grader.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+#include <string>
+
+using namespace std;
+
+getAnswers(, array[]);
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
